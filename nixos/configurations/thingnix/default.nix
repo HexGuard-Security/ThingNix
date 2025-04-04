02cb912f13b29c4df1f1bcb38eed1c801d4391f8
HexGuard Developer <developer@hexguard.net>
2025-04-04 19:28:44 +0530

    Fix deprecated nixFlakes reference by updating to nixVersions.stable

--- nixos/configurations/thingnix/default.nix
+++ nixos/configurations/thingnix/default.nix
@@ -66,7 +66,7 @@
 
   # Enable nix flakes
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nixVersions.stable;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
